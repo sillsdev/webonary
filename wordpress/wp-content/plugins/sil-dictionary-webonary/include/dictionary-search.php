e83756f03834a303e281f5455e89988be4a383e9
phil4literacy <philipperry@Philip.local>
2016-11-21 13:06:44 +0800

    v.6.3.1

--- wordpress/wp-content/plugins/sil-dictionary-webonary/include/dictionary-search.php
+++ wordpress/wp-content/plugins/sil-dictionary-webonary/include/dictionary-search.php
@@ -135,9 +135,7 @@ function sil_dictionary_custom_join($join) {
 				$collate = "";
 			}
 			
-			$subquery_where .= "(" . $search_table_name . ".search_strings LIKE '" . addslashes($letter) . "%' " . $collate .
-			" OR " . $search_table_name . ".search_strings LIKE '" . addslashes(strtoupper($letter)) . "%' " . $collate .
-			" OR " . $search_table_name . ".search_strings LIKE '" . addslashes("-" . $letter) . "%' " . $collate . ") " .
+			$subquery_where .= "(" . $search_table_name . ".search_strings REGEXP '^(=|-|\\\*|~)?[" . addslashes($letter) . addslashes(strtoupper($letter)) . "]' " . $collate . ")" .
 			" AND relevance >= 95 AND language_code = '$key' ";
 
 			$arrNoLetters = explode(",",  $noletters);
