afe1f4a7264aed4b9fdf1399d65f0dba5633cd43
Webonary <webonary@sil.org>
2017-06-06 15:10:04 +0800

    added Lipo translations

--- wordpress/wp-content/plugins/sil-dictionary-webonary/sil-dictionary.php
+++ wordpress/wp-content/plugins/sil-dictionary-webonary/sil-dictionary.php
@@ -9,7 +9,7 @@ Author: SIL International
 Author URI: http://www.sil.org/
 Text Domain: sil_dictionary
 Domain Path: /lang/
-Version: v. 6.5.9
+Version: v. 6.6.0
 License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
