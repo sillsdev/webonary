540637068c3e076118eca29cb544de1c816d481b
Webonary <webonary@sil.org>
2017-09-18 16:56:00 +0800

    added font BJCree UNI to repository

--- wordpress/wp-content/plugins/sil-dictionary-webonary/include/fonts.php
+++ wordpress/wp-content/plugins/sil-dictionary-webonary/include/fonts.php
@@ -8,23 +8,23 @@ class fontMonagment
 		$arrFontName[0] = null;
 		$arrFontStorage[0] = null;
 		$arrHasSubFonts[0] = null;
-		
+
 		$arrFontName[1] = "Charis SIL";
 		$arrFontStorage[1] = "CharisSIL";
 		$arrHasSubFonts[1] = true;
-		
+
 		$arrFontName[2] = "Charis SIL Compact";
 		$arrFontStorage[2] = "CharisSIL";
 		$arrHasSubFonts[2] = true;
-		
+
 		$arrFontName[3] = "Andika";
 		$arrFontStorage[3] = "Andika";
 		$arrHasSubFonts[3] = true;
-		
+
 		$arrFontName[4] = "Andika Compact";
 		$arrFontStorage[4] = "Andika";
 		$arrHasSubFonts[4] = true;
-		
+
 		$arrFontName[5] = "Ezra SIL";
 		$arrFontStorage[5] = "EzraSIL";
 		$arrHasSubFonts[5] = false;
@@ -32,11 +32,11 @@ class fontMonagment
 		$arrFontName[6] = "Galatia SIL";
 		$arrFontStorage[6] = "GalatiaSIL";
 		$arrHasSubFonts[6] = false;
-		
+
 		$arrFontName[7] = "Charis SIL Afr";
 		$arrFontStorage[7] = "CharisSILAfr";
 		$arrHasSubFonts[7] = false;
-		
+
 		$arrFontName[8] = "Charis SIL Am";
 		$arrFontStorage[8] = "CharisSILAm";
 		$arrHasSubFonts[8] = false;
@@ -64,19 +64,19 @@ class fontMonagment
 		$arrFontName[13] = "Charis SIL Phon";
 		$arrFontStorage[13] = "CharisSILPhon";
 		$arrHasSubFonts[13] = false;
-		
+
 		$arrFontName[14] = "Charis SIL Viet";
 		$arrFontStorage[14] = "CharisSILViet";
 		$arrHasSubFonts[14] = false;
-		
+
 		$arrFontName[15] = "Andika Afr";
 		$arrFontStorage[15] = "AndikaAfr";
 		$arrHasSubFonts[15] = false;
-		
+
 		$arrFontName[16] = "Andika Am";
 		$arrFontStorage[16] = "AndikaAm";
 		$arrHasSubFonts[16] = false;
-		
+
 		$arrFontName[17] = "Andika APac";
 		$arrFontStorage[17] = "AndikaAPac";
 		$arrHasSubFonts[17] = false;
@@ -88,11 +88,11 @@ class fontMonagment
 		$arrFontName[19] = "Andika CyrE";
 		$arrFontStorage[19] = "AndikaCyrE";
 		$arrHasSubFonts[19] = false;
-		
+
 		$arrFontName[20] = "Andika Eur";
 		$arrFontStorage[20] = "AndikaEur";
 		$arrHasSubFonts[20] = false;
-		
+
 		$arrFontName[21] = "Andika Phon";
 		$arrFontStorage[21] = "AndikaPhon";
 		$arrHasSubFonts[21] = false;
@@ -100,7 +100,7 @@ class fontMonagment
 		$arrFontName[22] = "Andika Viet";
 		$arrFontStorage[22] = "AndikaViet";
 		$arrHasSubFonts[22] = false;
-		
+
 		$arrFontName[23] = "Doulos SIL";
 		$arrFontStorage[23] = "DoulosSIL";
 		$arrHasSubFonts[23] = true;
@@ -128,7 +128,7 @@ class fontMonagment
 		$arrFontName[29] = "Doulos SIL Eur";
 		$arrFontStorage[29] = "DoulosSILEur";
 		$arrHasSubFonts[29] = false;
-		
+
 		$arrFontName[30] = "Doulos SIL Phon";
 		$arrFontStorage[30] = "DoulosSILPhon";
 		$arrHasSubFonts[30] = false;
@@ -140,7 +140,7 @@ class fontMonagment
 		$arrFontName[32] = "Annapurna SIL";
 		$arrFontStorage[32] = "AnnapurnaSIL";
 		$arrHasSubFonts[32] = false;
-		
+
 		$arrFontName[33] = "Annapurna SIL Nepal";
 		$arrFontStorage[33] = "AnnapurnaSILNepal";
 		$arrHasSubFonts[33] = false;
@@ -148,11 +148,11 @@ class fontMonagment
 		$arrFontName[34] = "Abyssinica SIL";
 		$arrFontStorage[34] = "AbyssinicaSIL";
 		$arrHasSubFonts[34] = false;
-		
+
 		$arrFontName[35] = "Scheherazade";
 		$arrFontStorage[35] = "Scheherazade";
 		$arrHasSubFonts[35] = false;
-		
+
 		$arrFontName[36] = "Padauk";
 		$arrFontStorage[36] = "Padauk";
 		$arrHasSubFonts[36] = false;
@@ -160,12 +160,16 @@ class fontMonagment
 		$arrFontName[37] = "Nokyung";
 		$arrFontStorage[37] = "Nokyung.ttf";
 		$arrHasSubFonts[37] = false;
-		
+
 		$arrFontName[38] = "Taogu";
 		$arrFontStorage[38] = "Taogu.ttf";
 		$arrHasSubFonts[38] = false;
+
+		$arrFontName[39] = "BJCree UNI";
+		$arrFontStorage[39] = "Bycrus.woff";
+		$arrHasSubFonts[39] = false;
 	}
-	
+
 	public function get_fonts_fromCssText($css_string)
 	{
 		// Get the CSS that contains a font-family rule.
@@ -181,7 +185,7 @@ class fontMonagment
 				$end = $length;
 			}
 			$end++;
-			 
+
 			// Get position of the last closing bracket (start of this section).
 			$start = strrpos($css_string, '}', - ($length - $last_position));
 			if ($start === FALSE) {
@@ -190,21 +194,21 @@ class fontMonagment
 			else {
 				$start++;
 			}
-			 
+
 			// Get closing ; in order to get the end of the declaration.
 			$declaration_end = strpos($css_string, ';', $last_position);
-			 
+
 			// Get values.
 			$start_of_values = strpos($css_string, ':', $last_position);
 			$values_string = substr($css_string, $start_of_values + 1, $declaration_end - ($start_of_values + 1));
 			// Parse values string into an array of values.
 			$values_array = explode(',', $values_string);
-			 
+
 			$fontName = trim(str_replace("'", "", $values_array[0]));
 			$arrCSSFonts[$x] = str_replace('"', '', $fontName);
-			 
+
 			// Values array has more than 1 value and first element is a quoted string.
-			 
+
 			// Advance position.
 			$x++;
 			$last_position = $end;
@@ -213,17 +217,17 @@ class fontMonagment
 		if(count($arrCSSFonts) > 0)
 		{
 			$arrUniqueCSSFonts = array_unique($arrCSSFonts);
-			
+
 			sort($arrUniqueCSSFonts);
 		}
-		 
+
 		return $arrUniqueCSSFonts;
 	}
-	
+
 	public function get_system_fonts()
 	{
 		$arrSystemFonts = array("Arial", "Arial Black", "Helvetica", "Times New Roman", "SimSun", "Tahoma");
-		
+
 		return  $arrSystemFonts;
 	}
 
@@ -234,11 +238,11 @@ class fontMonagment
 	{
 		$css_string = file_get_contents($file);
 		$arrUniqueCSSFonts = $this->get_fonts_fromCssText($css_string);
-		
+
 		$this->getFontsAvailable($arrName, $arrStorage, $arrHasSubFonts);
 		$arrFontName = $arrName;
 		$arrFontStorage = $arrStorage;
-		 
+
 		$fontFace = "";
 		$arrFontStyles = array("R", "B", "I", "BI");
 		if(count($arrUniqueCSSFonts) > 0)
@@ -246,7 +250,7 @@ class fontMonagment
 			foreach($arrUniqueCSSFonts as $userFont)
 			{
 				$fontKey = array_search($userFont, $arrFontName);
-					
+
 				if($fontKey > 0)
 				{
 					foreach($arrFontStyles as $fontStyle)
@@ -258,7 +262,7 @@ class fontMonagment
 							$arrFontStorage[$fontKey] = str_replace(".ttf", "", $arrFontStorage[$fontKey]);
 							$extension = ".ttf";
 						}
-						
+
 						if(file_exists(WP_CONTENT_DIR . "/uploads/fonts/" . $arrFontStorage[$fontKey] . "-" . $fontStyle . $extension))
 						{
 							$fontFace .= "@font-face {\n";
