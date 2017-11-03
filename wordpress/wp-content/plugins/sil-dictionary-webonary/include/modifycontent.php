f3ec26699a09a62c65969eccfe1f301bfc854e85
Webonary <webonary@sil.org>
2017-11-03 10:51:15 +0800

    fixed modifycontent to preserve whitespace

--- wordpress/wp-content/plugins/sil-dictionary-webonary/include/modifycontent.php
+++ wordpress/wp-content/plugins/sil-dictionary-webonary/include/modifycontent.php
@@ -4,6 +4,8 @@ function addLangQuery($content)
 	if(isset($_GET['lang']))
 	{
 		$doc = new DOMDocument();
+		$doc->preserveWhitespace = true;
+		$doc->formatOutput = false;
 
 		// load the string into the DOM (this is your page's HTML), see below for more info
 		$doc->loadHTML(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'));
