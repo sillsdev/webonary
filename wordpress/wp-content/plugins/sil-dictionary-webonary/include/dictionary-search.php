b9f6b10ff48a361330e1fa995977013df40be107
phil4literacy <philip@learnclick.com>
2018-05-21 12:58:28 +0800

    v. 7.3.9

--- wordpress/wp-content/plugins/sil-dictionary-webonary/include/dictionary-search.php
+++ wordpress/wp-content/plugins/sil-dictionary-webonary/include/dictionary-search.php
@@ -517,7 +517,7 @@ function filter_the_content_in_the_main_loop( $content ) {
 	$content = normalizer_normalize($content, Normalizer::NFC );
 	return $content;
 }
-add_filter( 'the_content', 'filter_the_content_in_the_main_loop' );
+//add_filter( 'the_content', 'filter_the_content_in_the_main_loop' );
 
 function webonary_css()
 {
