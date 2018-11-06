6502ff914e91d87cb83978742253de79fee4d17c
phil4literacy <philip@learnclick.com>
2018-11-06 16:30:52 +0800

    v. 7.7.2

--- wordpress/wp-content/plugins/sil-dictionary-webonary/include/api.php
+++ wordpress/wp-content/plugins/sil-dictionary-webonary/include/api.php
@@ -81,7 +81,7 @@ class Webonary_API_MyType {
     public function import2(WP_REST_Request $request)
     {
     	echo $headers = $request->get_headers();
-    	echo $headers['authorization'] . "\n";
+    	echo $headers['authorization'][0] . "\n";
     	echo base64_decode($headers['authorization']) . "\n";
     	//$this->import($request);
     }
