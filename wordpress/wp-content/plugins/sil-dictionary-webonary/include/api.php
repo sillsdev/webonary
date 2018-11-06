9c69528692dc3da4cc5af0dab3d5b726edca7bd4
phil4literacy <philip@learnclick.com>
2018-11-06 13:18:07 +0800

    v. 7.7.2

--- wordpress/wp-content/plugins/sil-dictionary-webonary/include/api.php
+++ wordpress/wp-content/plugins/sil-dictionary-webonary/include/api.php
@@ -85,7 +85,6 @@ class Webonary_API_MyType {
 
 	public function import($_headers)
 	{
-		echo var_dump($_headers);
 		$authenticated = $this->verifyAdminPrivileges($email, $userid);
 
 		$message = "The export to Webonary is completed.\n";
