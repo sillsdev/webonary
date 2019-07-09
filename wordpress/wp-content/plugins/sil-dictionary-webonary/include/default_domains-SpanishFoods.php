a96b3d0e4e90fb92021e74e8ada3adacc75a412a
phil4literacy <philip@learnclick.com>
2019-07-09 16:36:41 +0100

    v. 8.2.6

--- wordpress/wp-content/plugins/sil-dictionary-webonary/include/default_domains-SpanishFoods.php
+++ wordpress/wp-content/plugins/sil-dictionary-webonary/include/default_domains-SpanishFoods.php
@@ -1,13 +1,13 @@
 <?php
-$defaultDomain['1.'] = __('FOODS');
-$defaultDomain['1.1.'] = __('Cereal');
-$defaultDomain['1.2.'] = __('Creole food');
-$defaultDomain['1.3.'] = __('Dessert');
-$defaultDomain['1.4.'] = __('Fish');
-$defaultDomain['1.5.'] = __('Fruit');
-$defaultDomain['1.6.'] = __('Meat');
-$defaultDomain['1.7.'] = __('Puerto Rican Fritters');
-$defaultDomain['1.8.'] = __('Seafood');
-$defaultDomain['1.9.'] = __('Spice');
-$defaultDomain['1.10.'] = __('Vegetables');
+$defaultDomain['1.'] = __('FOODS', 'sil_dictionary');
+$defaultDomain['1.1.'] = __('Cereal', 'sil_dictionary');
+$defaultDomain['1.2.'] = __('Creole food', 'sil_dictionary');
+$defaultDomain['1.3.'] = __('Dessert', 'sil_dictionary');
+$defaultDomain['1.4.'] = __('Fish', 'sil_dictionary');
+$defaultDomain['1.5.'] = __('Fruit', 'sil_dictionary');
+$defaultDomain['1.6.'] = __('Meat', 'sil_dictionary');
+$defaultDomain['1.7.'] = __('Puerto Rican Fritters', 'sil_dictionary');
+$defaultDomain['1.8.'] = __('Seafood', 'sil_dictionary');
+$defaultDomain['1.9.'] = __('Spice', 'sil_dictionary');
+$defaultDomain['1.10.'] = __('Vegetables', 'sil_dictionary');
 ?>
