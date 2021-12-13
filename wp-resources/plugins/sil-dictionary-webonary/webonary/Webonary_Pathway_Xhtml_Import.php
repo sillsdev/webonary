ea435cd36b9f9d657fbc39e795c449ce18975b39
Phil Hopper <phopper@rvwholesalers.com>
2021-12-13 12:22:24 -0500

    Fix upload from FLEx 9.0

--- wp-resources/plugins/sil-dictionary-webonary/webonary/Webonary_Pathway_Xhtml_Import.php
+++ wp-resources/plugins/sil-dictionary-webonary/webonary/Webonary_Pathway_Xhtml_Import.php
@@ -3,6 +3,17 @@
 
 use Overtrue\Pinyin\Pinyin;
 
+
+// Include the WordPress Importer.
+include_once ABSPATH . 'wp-admin/includes/import.php';
+
+if (!class_exists('WP_Importer')) {
+	$class_wp_importer = ABSPATH . 'wp-admin/includes/class-wp-importer.php';
+	if (file_exists($class_wp_importer))
+		include_once $class_wp_importer;
+}
+
+
 class Webonary_Pathway_Xhtml_Import extends WP_Importer
 {
 	public $api = false; //if data is sent from an external program
