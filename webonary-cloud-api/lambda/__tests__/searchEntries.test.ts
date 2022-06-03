0f57b9aba3a872b6ea006a4f5ed62ae8aca2bc54
john <wc.john.tseng@gmail.com>
2022-06-03 13:41:20 -0500

    Refactor mongo test setup into a separate file so it can be reused in other tests.

--- webonary-cloud-api/lambda/__tests__/searchEntries.test.ts
+++ webonary-cloud-api/lambda/__tests__/searchEntries.test.ts
@@ -1,32 +1,9 @@
-// It's there in devDependencies. Not sure why it's not working.
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import { searchEntries, SearchEntriesArguments } from '../searchEntries';
-import { upsertDictionary } from '../postDictionary';
 
 import { upsertEntries } from '../postEntry';
+import { createDictionary, setupMongo } from './databaseSetup';
 
-let mongoServer: MongoMemoryServer;
-
-beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  process.env.DB_URL = mongoServer.getUri();
-});
-
-afterAll(async () => {
-  await mongoServer.stop();
-});
-
-let nextDictionaryNumber = 1;
-/** Inserts a new dictionary into the database and returns its dictionaryId. */
-async function createDictionary(): Promise<string> {
-  const dictionaryId = `test-dictionary-${nextDictionaryNumber}`;
-  nextDictionaryNumber += 1;
-
-  await upsertDictionary('{}', dictionaryId, 'test-user');
-
-  return dictionaryId;
-}
+setupMongo();
 
 const defaultArguments: SearchEntriesArguments = {
   $language: '',
