52b959fe907c4e0713d32f7e6ad9f43b435c7774
John Tseng <wc.john.tseng@gmail.com>
2022-11-02 14:13:59 +0000

    Move error checking up to reduce nesting.

--- webonary-cloud-api/lambda/methodAuthorize.ts
+++ webonary-cloud-api/lambda/methodAuthorize.ts
@@ -38,46 +38,46 @@ export async function handler(
   const authHeaders = event.headers?.Authorization;
   const dictionaryId = event.pathParameters?.dictionaryId;
 
-  if (dictionaryId && authHeaders) {
-    const credentials = getBasicAuthCredentials(authHeaders);
-
-    // eslint-disable-next-line no-console
-    console.log(`Processing policy for ${credentials.username} to resource ${event.methodArn}`);
-    // Same user should have the same access to post/delete dictionary entry data as well as files.
-    const principalId = credentials.username;
-    const resourceRegex = /(POST\/post|DELETE\/delete)\/(dictionary|entry|file)\/(.+)$/i;
-
-    // Call Webonary.org for user authentication
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
-    const authPath = `${process.env.WEBONARY_URL}/${dictionaryId}${process.env.WEBONARY_AUTH_PATH}`;
-
-    try {
-      const response = await axios.post(authPath, '{}', {
-        auth: credentials,
-      });
-
-      if (response.status === 200 && response.data) {
-        const resources = response.data.split(',').map((id: string) => {
-          // To allow for correct caching behavior, we use wildcards for method (POST or DELETE) and path. E.g.:
-          // arn:aws:execute-api:region:zz:zzz/prod/POST/post/dictionary/myDictionary will be replaced with
-          // arn:aws:execute-api:region:zz:zzz/prod/*/*/dictionary/myDictionary
-          return event.methodArn.replace(resourceRegex, `*/*/${id}`);
-        });
-
-        // eslint-disable-next-line no-console
-        console.log(`Creating policy for ${principalId} to access ${resources}`);
-        return callback(null, generatePolicy(principalId, 'Allow', resources));
-      }
-    } catch (error) {
-      const resources = [event.methodArn.replace(resourceRegex, `*/*/${dictionaryId}`)];
-
-      // eslint-disable-next-line no-console
-      console.log(`Denying ${principalId} to access {resources}`);
-      return callback(null, generatePolicy(principalId, 'Deny', resources)); // 403
-    }
+  if (!dictionaryId || !authHeaders) {
+    return callback('Unauthorized');
   }
 
-  return callback('Unauthorized');
+  const credentials = getBasicAuthCredentials(authHeaders);
+
+  // eslint-disable-next-line no-console
+  console.log(`Processing policy for ${credentials.username} to resource ${event.methodArn}`);
+  // Same user should have the same access to post/delete dictionary entry data as well as files.
+  const principalId = credentials.username;
+  const resourceRegex = /(POST\/post|DELETE\/delete)\/(dictionary|entry|file)\/(.+)$/i;
+
+  // Call Webonary.org for user authentication
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
+  const authPath = `${process.env.WEBONARY_URL}/${dictionaryId}${process.env.WEBONARY_AUTH_PATH}`;
+
+  try {
+    const response = await axios.post(authPath, '{}', {
+      auth: credentials,
+    });
+
+    if (response.status === 200 && response.data) {
+      const resources = response.data.split(',').map((id: string) => {
+        // To allow for correct caching behavior, we use wildcards for method (POST or DELETE) and path. E.g.:
+        // arn:aws:execute-api:region:zz:zzz/prod/POST/post/dictionary/myDictionary will be replaced with
+        // arn:aws:execute-api:region:zz:zzz/prod/*/*/dictionary/myDictionary
+        return event.methodArn.replace(resourceRegex, `*/*/${id}`);
+      });
+
+      // eslint-disable-next-line no-console
+      console.log(`Creating policy for ${principalId} to access ${resources}`);
+      return callback(null, generatePolicy(principalId, 'Allow', resources));
+    }
+  } catch (error) {
+    const resources = [event.methodArn.replace(resourceRegex, `*/*/${dictionaryId}`)];
+
+    // eslint-disable-next-line no-console
+    console.log(`Denying ${principalId} to access {resources}`);
+    return callback(null, generatePolicy(principalId, 'Deny', resources)); // 403
+  }
 }
 
 export default handler;
