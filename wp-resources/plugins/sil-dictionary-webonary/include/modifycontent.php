08cd0e4814d4e51965c48c063438d438fa1b84cc
Phillip Hopper <phillip_hopper@sil.org>
2024-09-21 11:57:43 -0400

    Tweaks

--- wp-resources/plugins/sil-dictionary-webonary/include/modifycontent.php
+++ wp-resources/plugins/sil-dictionary-webonary/include/modifycontent.php
@@ -12,22 +12,8 @@ function addLangQuery($content): string
 	libxml_use_internal_errors(true);
 	$doc->loadHTML(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'));
 
-	if ($lang !== false) {
-
-		//Loop through each <a> tag in the dom and change the href property
-		foreach ($doc->getElementsByTagName('a') as $anchor) {
-
-			$link = $anchor->getAttribute('href');
-
-			if (strpos($link, "?") > 0)
-				$link .= '&lang=' . $lang;
-			else
-				$link .= '?lang=' . $lang;
-
-			if (!strpos($link, ".apk"))
-				$anchor->setAttribute('href', $link);
-		}
-	}
+	if ($lang !== false)
+		ProcessHrefs($doc, $lang);
 
 	// render the content
 	$content = $doc->saveHTML();
@@ -35,6 +21,46 @@ function addLangQuery($content): string
 	// return the innerHTML of the <body> tag
 	$re = '/^.*?<body>(.*)?<\/body>.*$/s';
 	return preg_replace($re, '$1', $content);
+}
+
+/**
+ * Loop through the document, adding the lang to the href query string
+ *
+ * @param DOMDocument $doc
+ * @param string $lang
+ * @return void
+ */
+function ProcessHrefs(DOMDocument $doc, string $lang): void
+{
+	//Loop through each <a> tag in the dom and change the href property
+	foreach ($doc->getElementsByTagName('a') as $anchor) {
+
+		$link = $anchor->getAttribute('href');
+
+		// skip this for .apk links
+		if (str_contains($link, '.apk'))
+			continue;
+
+		$parts = explode('?', $link);
+
+		if (count($parts) > 1) {
+
+			// remove any existing lang values
+			$qs_parts = array_filter(
+				explode('&', $parts[1]),
+				function ($val) { return !str_starts_with($val, 'lang='); }
+			);
+		}
+		else {
+			$qs_parts = [];
+		}
+
+		$qs_parts[] = 'lang=' . $lang;
+
+		$link = $parts[0] . '?' . implode('&', $qs_parts);
+
+		$anchor->setAttribute('href', $link);
+	}
 }
 
 remove_filter('the_content', 'wptexturize');
