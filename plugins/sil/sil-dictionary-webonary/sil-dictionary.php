7866c5707471a6823604dc587c2647bd026f809e
Phillip Hopper <phillip_hopper@sil.org>
2025-11-06 20:21:33 -0500

    WIP

--- plugins/sil/sil-dictionary-webonary/sil-dictionary.php
+++ plugins/sil/sil-dictionary-webonary/sil-dictionary.php
@@ -49,46 +49,7 @@ global $wpdb, $search_cookie;
 
 Main::Run();
 
-add_action('init', 'Webonary_Utility::LoadTextDomains');
 
-// see: https://www.monsterinsights.com/docs/how-to-disable-the-monsterinsights-dashboard-widget/
-// Had to put this here rather than in the theme because the MonsterInsights plugin is
-// already loaded before the theme loads.
-add_filter('monsterinsights_show_dashboard_widget', '__return_false');
-
-//if(is_admin() ){
-	// Menu in the WordPress Dashboard, under tools.
-	add_action('admin_menu', 'Webonary_Configuration::add_admin_menu');
-	add_action('admin_bar_menu', 'Webonary_Configuration::on_admin_bar', 35);
-
-	// I looked for a register_install_hook, but given the way WordPress plugins
-	// can be implemented, I'm not sure it would work right even if I did find one.
-	// The register_activation_hook() appears not to work for some reason. But the
-	// site won't start up that much any way, and it doesn't hurt anything to call
-	// it more than once.
-	add_action('init', 'Webonary_Infrastructure::InstallInfrastructure', 0);
-//}
-
-
-/* Search hook */
-
-/* post query hooks */
-add_filter('posts_request','replace_default_search_filter', 10, 2);
-
-// be sure these style sheets are loaded last, after the theme
-add_action('wp_enqueue_scripts', 'Webonary_Utility::EnqueueJsAndCss', 999991);
-
-// this executes just before wordpress determines which template page to load
-add_action('after_setup_theme', 'Webonary_SearchCookie::GetSearchCookie');
-
-// add_action('pre_get_posts','no_standard_sort');
-add_action('preprocess_comment' , 'preprocess_comment_add_type');
-
-// API for FLEx
-add_action('rest_api_init', 'Webonary_API_MyType::Register_New_Routes');
-
-// API for Webonary Cloud API
-add_action('rest_api_init', 'Webonary_Cloud::registerApiRoutes');
 
 // Block all API requests from users not logged in, with exceptions
 // See https://developer.wordpress.org/rest-api/frequently-asked-questions/#require-authentication-for-all-requests
@@ -123,19 +84,6 @@ add_filter('rest_authentication_errors', function ($result) {
 	);
 });
 
-// NOTE: this was removed because appears to be applying the vernacular settings to the UI language (which it shouldn't)
-//// add the correct RTL/LTR class
-//function filter_post_class($classes)
-//{
-//	$rtl = get_option('vernacularRightToLeft') == '1';
-//	$align_class = $rtl ? 'right' : 'left';
-//
-//	if (!in_array($align_class, $classes))
-//		$classes[] = $align_class;
-//
-//	return $classes;
-//}
-//add_filter('post_class', 'filter_post_class', 10, 3);
 
 if (IS_CLOUD_BACKEND) {
 	add_filter('posts_pre_query', 'Webonary_Cloud::searchEntries', 10, 2);
--- plugins/sil/sil-dictionary-webonary/src/Hooks.php
+++ plugins/sil/sil-dictionary-webonary/src/Hooks.php
@@ -10,7 +10,7 @@ class Hooks
 			return 0;
 
 		$hooks_set = self::SetAdminHooks();
-//		$hooks_set += self::SetAllPageHooks();
+		$hooks_set += self::SetAllPageHooks();
 //		$hooks_set += self::SetDictionaryHooks();
 
 		return $hooks_set;
@@ -28,8 +28,9 @@ class Hooks
 
 		// only show these for dictionary sites, not the main site
 		if ($blog_id > 1) {
-//			$hooks_set += (int)add_action('admin_menu', 'SIL\Webonary\Admin::SetAdminMenu');
-//			$hooks_set += (int)add_action('admin_bar_menu', 'SIL\Webonary\Admin::SetAdminBar', 35);
+			$hooks_set += (int)add_action('admin_menu', 'SIL\Webonary\Admin::SetAdminMenu');
+			$hooks_set += (int)add_action('admin_bar_menu', 'SIL\Webonary\Admin::SetAdminBar', 35);
+
 //			$hooks_set += (int)add_action('wp_dashboard_setup', 'SIL\Webonary\DashboardWidget::AddWidget');
 		}
 
@@ -43,5 +44,37 @@ class Hooks
 		$hooks_set += (int)add_filter('monsterinsights_show_dashboard_widget', '__return_false');
 
 		return $hooks_set;
+	}
+
+	private static function SetAllPageHooks(): int
+	{
+		$hooks_set = 0;
+
+		$hooks_set += (int)add_action('init', 'SIL\Webonary\Hooks::LoadAdditionalTextDomains');
+		$hooks_set += (int)add_action('init', 'Webonary_Infrastructure::InstallInfrastructure', 0);
+		$hooks_set += (int)add_filter('posts_request', 'replace_default_search_filter', 10, 2);
+
+		// be sure these style sheets are loaded last, after the theme
+		$hooks_set += (int)add_action('wp_enqueue_scripts', 'Webonary_Utility::EnqueueJsAndCss', 999991);
+
+		// this executes just before wordpress determines which template page to load
+		$hooks_set += (int)add_action('after_setup_theme', 'Webonary_SearchCookie::GetSearchCookie');
+
+		$hooks_set += (int)add_action('preprocess_comment' , 'preprocess_comment_add_type');
+		$hooks_set += (int)add_action('rest_api_init', 'Webonary_API_MyType::Register_New_Routes');
+		$hooks_set += (int)add_action('rest_api_init', 'Webonary_Cloud::registerApiRoutes');
+
+//		$hooks_set += (int)add_action('switch_blog', 'SIL\Webonary\Dictionaries::BlogWasSwitched');
+
+		return $hooks_set;
+	}
+
+	/**
+	 * @return void
+	 */
+	public static function LoadAdditionalTextDomains(): void
+	{
+		$include_dir = 'sil-dictionary-webonary/include';
+		load_plugin_textdomain('sil_domains', false, $include_dir . '/sem-domains');
 	}
 }
