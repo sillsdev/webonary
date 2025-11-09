b5aa9480c601ce6a6d5ca4abc677a1e215098183
Phillip Hopper <phillip_hopper@sil.org>
2025-11-08 20:36:41 -0500

    WIP

--- plugins/sil/sil-dictionary-webonary/src/Hooks.php
+++ plugins/sil/sil-dictionary-webonary/src/Hooks.php
@@ -31,17 +31,17 @@ class Hooks
 		$hooks_set = 0;
 		$blog_id = get_current_blog_id();
 
-		$hooks_set += (int)add_action('admin_enqueue_scripts', 'SIL\Webonary\Admin::EnqueueAdminScripts');
+		$hooks_set += (int)add_action('admin_enqueue_scripts', [Admin::class, 'EnqueueAdminScripts']);
 
 		// only show these for dictionary sites, not the main site
 		if ($blog_id > 1) {
-			$hooks_set += (int)add_action('admin_menu', 'SIL\Webonary\Admin::SetAdminMenu');
-			$hooks_set += (int)add_action('admin_bar_menu', 'SIL\Webonary\Admin::SetAdminBar', 35);
+			$hooks_set += (int)add_action('admin_menu', [Admin::class, 'SetAdminMenu']);
+			$hooks_set += (int)add_action('admin_bar_menu', [Admin::class, 'SetAdminBar'], 35);
 
 //			$hooks_set += (int)add_action('wp_dashboard_setup', 'SIL\Webonary\DashboardWidget::AddWidget');
 		}
 
-//		$hooks_set += (int)add_action('network_admin_menu', 'SIL\Webonary\Admin::SetAdminMultisiteMenu');
+		$hooks_set += (int)add_action('network_admin_menu', [Admin::class, 'AddLanguageProblemMenuItem']);
 
 		/**
 		 * see: https://www.monsterinsights.com/docs/how-to-disable-the-monsterinsights-dashboard-widget/
@@ -74,6 +74,16 @@ class Hooks
 		// Block all API requests from users not logged in, with exceptions
 		// See https://developer.wordpress.org/rest-api/frequently-asked-questions/#require-authentication-for-all-requests
 		$hooks_set += (int)add_filter('rest_authentication_errors', [Hooks::class, 'ApplyRestAuthenticationExceptions']);
+
+		if (IS_CLOUD_BACKEND) {
+			$hooks_set += (int)add_filter('posts_pre_query', [Webonary_Cloud::class, 'searchEntries'], 10, 2);
+			$hooks_set += (int)add_filter('comment_post_redirect', [Webonary_Cloud::class, 'commentRedirect']);
+		}
+
+		$hooks_set += (int)add_filter('post_rewrite_rules', [Hooks::class, 'AddRewriteRules']);
+		$hooks_set += (int)add_filter('query_vars', [Hooks::class, 'AddQueryVars']);
+		$hooks_set += (int)add_action('widgets_init', [Hooks::class, 'RegisterCustomWidgets']);
+		$hooks_set += (int)add_filter('shortcode_atts_audio', [Hooks::class, 'FixAudioFileNames']);
 
 //		$hooks_set += (int)add_action('switch_blog', 'SIL\Webonary\Dictionaries::BlogWasSwitched');
 
@@ -118,5 +128,70 @@ class Hooks
 			__('This API can only be called if you are logged in first.'),
 			array('status' => 401)
 		);
+	}
+
+	public static function AddRewriteRules($rules): array
+	{
+		$new_rule = ['^/([^/]+)/?$' => 'index.php?clean=$matches[1]'];
+		return $new_rule + $rules;
+	}
+
+	public static function AddQueryVars($query_vars): array
+	{
+		if (!in_array('clean', $query_vars))
+			$query_vars[] = 'clean';
+
+		if (!in_array('semdomain', $query_vars))
+			$query_vars[] = 'semdomain';
+
+		if (!in_array('semnumber', $query_vars))
+			$query_vars[] = 'semnumber';
+
+		return $query_vars;
+	}
+
+	public static function RegisterCustomWidgets(): void
+	{
+		register_widget('Webonary_Search_Widget');
+		register_widget('Webonary_Published_Widget');
+	}
+
+	public static function FixAudioFileNames($out)
+	{
+		// the array key we need is either a file extension or "src"
+		$audio_types = wp_get_audio_extensions();
+		$audio_types[] = 'src';
+
+		// check if this audio short code contains a file name
+		foreach ($audio_types as $type) {
+
+			if (empty($out[$type]))
+				continue;
+
+			if (!is_string($out[$type]))
+				continue;
+
+			// check if the file name contains HTML encoded text
+			if (str_contains($out[$type], '&#')) {
+
+				$parts = explode('/', $out[$type]);
+				$changed = false;
+
+				foreach ($parts as $key => $part) {
+
+					// decode the text into unicode characters
+					if (str_contains($part, '&#')) {
+						$parts[$key] = html_entity_decode($part);
+						$changed = true;
+					}
+				}
+
+				// the browser will URL encode the file name if needed
+				if ($changed)
+					$out[$type] = implode('/', $parts);
+			}
+		}
+
+		return $out;
 	}
 }
