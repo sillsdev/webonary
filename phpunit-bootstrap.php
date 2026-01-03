d1f8a9f20cef5e0526e208aa93b6b3789920d63c
Phillip Hopper <phillip_hopper@sil.org>
2026-01-02 22:41:59 -0500

    WIP - unit testing

--- phpunit-bootstrap.php
+++ phpunit-bootstrap.php
@@ -5,6 +5,7 @@ const TESTS_DIR = __DIR__ . '/test-php';
 const CONFIG_DIR = TESTS_DIR . '/config';
 const TEST_RESOURCES = TESTS_DIR . '/resources';
 const WEBONARY_CLOUD_DEFAULT_DICTIONARY_ID = 'unit_test';
+const WEBONARY_CLOUD_API_URL = 'https://unit-test.local/v1/';
 
 register_shutdown_function(function(){
     print 'SHUTDOWN' . PHP_EOL;
